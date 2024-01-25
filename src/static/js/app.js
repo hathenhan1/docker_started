b01cf3a95280eb2cd9c8c348e4cde716182d62ae
hathenhan1 <hathenhanfpt@gmail.com>
2024-01-25 13:31:11 +0700

    new update

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add some project'}
+                        {submitting ? 'Adding...' : 'Add new things'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
